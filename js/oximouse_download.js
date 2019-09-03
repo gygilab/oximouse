a2863b837604b8f68fa61cd52e78e13cc461ab12
dschweppe <devin.k.schweppe@gmail.com>
2019-09-02 22:35:30 -0400

    Additional HP data for download but not display

--- js/oximouse_download.js
+++ js/oximouse_download.js
@@ -4,10 +4,18 @@
 /// Authors, Gygi Lab, President and Fellows of Harvard University
 ///
 
-
-$('#downloadSites').attr({action: 'data/site_all.csv'});// Sites downloads
-
+var downloaderFormaterVariable;
+/**
+ * Sites downloads
+ */
+$('#downloadSites').attr({action: 'data/site_all.csv'});
 // Communities downloads:
 $('#downloadCommunities').attr({action: 'data/communities/huttlin2017_communities_bioplex_overlay_percent.tsv'});
-
 $('#downloadCommunityOverlay').attr({action: 'data/communities/huttlin2017_communities_oxi_overlay.tsv'});
+
+
+//Supp data:
+$('#downloadOldSites').attr({action: 'data/download/All_old_sites.xlsx'});
+$('#downloadYoungSites').attr({action: 'data/download/All_young_sites.xlsx'});
+$('#downloadDiseaseYoung').attr({action: 'data/download/Disease_network_young.xlsx'});
+$('#downloadDiseaseOld').attr({action: 'data/download/Disease_network_old.xlsx'});
